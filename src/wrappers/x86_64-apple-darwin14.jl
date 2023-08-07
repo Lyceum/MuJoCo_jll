353ee72af61de986c9ccd4aecb28098eb8f743bb
nicBarbara <nbar5346@uni.sydney.edu.au>
2023-08-07 11:31:36 -0400

    Changed name from libmujuco to libmujoco

--- src/wrappers/x86_64-apple-darwin14.jl
+++ src/wrappers/x86_64-apple-darwin14.jl
@@ -1,8 +1,8 @@
 # Manually generated wrapper script for MuJoCo_jll for macos
-export libmujuco, mujoco_basic, mujoco_compile, mujoco_derivative, mujoco_record, mujoco_simulate, mujoco_testspeed, mujoco_testxml
+export libmujoco, mujoco_basic, mujoco_compile, mujoco_derivative, mujoco_record, mujoco_simulate, mujoco_testspeed, mujoco_testxml
 
 JLLWrappers.@generate_wrapper_header("MuJoCo")
-JLLWrappers.@declare_library_product(libmujuco, "libmujoco.2.3.7.dylib")
+JLLWrappers.@declare_library_product(libmujoco, "libmujoco.2.3.7.dylib")
 JLLWrappers.@declare_executable_product(mujoco_basic)
 JLLWrappers.@declare_executable_product(mujoco_compile)
 JLLWrappers.@declare_executable_product(mujoco_derivative)
@@ -13,7 +13,7 @@ JLLWrappers.@declare_executable_product(mujoco_testxml)
 function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
-        libmujuco,
+        libmujoco,
         "lib/libmujoco.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
